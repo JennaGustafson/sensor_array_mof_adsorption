ef2b4f6b4a546dd322710b965d4d7fbcc5c8681c
Jenna <jag227@pitt.edu>
2016-04-08 12:10:27 -0400

    refactored run script for gas mixtures

--- adsIRM.sh
+++ adsIRM.sh
@@ -18,19 +18,9 @@ export RASPA_DIR="/ihome/cwilmer/jag227/RASPA-2.0"
 		CH4_COMP=$(awk NR==$i CH4c)
 		N2_COMP=$(awk NR==$i N2c)
 		C2H6_COMP=$(awk NR==$i C2H6c)
-		
-		sed -i "20 s/[0-9]*\.[0-9]*/$CO2_COMP/" simulation.input
-		sed -i "31 s/[0-9]*\.[0-9]*/$CH4_COMP/" simulation.input
-		sed -i "42 s/[0-9]*\.[0-9]*/$N2_COMP/" simulation.input
-		sed -i "53 s/[0-9]*\.[0-9]*/$C2H6_COMP/" simulation.input
-		
-		cp simulation.input sim$i.input
-		mv sim$i.input inputs
-		./run.sh
-		mkdir Out$i
-		cp -r Output Out$i
-		mv Out$i outputs/
-		
+
+
+
 		if [ "$i" = 1 ]
 		then
 			grep Aver.*load Output/System_0/*00.data | awk NR==3 > co2IRMOF-1P1.txt
@@ -43,7 +33,7 @@ export RASPA_DIR="/ihome/cwilmer/jag227/RASPA-2.0"
                         grep Aver.*load Output/System_0/*6.data | awk NR==33 > c2h6IRMOF-1P10.txt
 
 		else
-		
+
 			grep Aver.*load Output/System_0/*00.data | awk NR==3 >> co2IRMOF-1P1.txt
 			grep Aver.*load Output/System_0/*00.data | awk NR==13 >> ch4IRMOF-1P1.txt
 			grep Aver.*load Output/System_0/*00.data | awk NR==23 >> n2IRMOF-1P1.txt
---
+++ run_ads_IRMOF1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+CO2_COMP=$1
+CH4_COMP=$2
+N2_COMP=$3
+C2H6_COMP=$4
+
+mkdir -p outputs
+mkdir -p inputs
+sed -i "20 s/[0-9]*\.[0-9]*/$CO2_COMP/" simulation.input
+sed -i "31 s/[0-9]*\.[0-9]*/$CH4_COMP/" simulation.input
+sed -i "42 s/[0-9]*\.[0-9]*/$N2_COMP/" simulation.input
+sed -i "53 s/[0-9]*\.[0-9]*/$C2H6_COMP/" simulation.input
+
+cp simulation.input sim_${CO2_COMP}_${CH4_COMP}_${N2_COMP}_${C2H6_COMP}.input
+mv sim${CO2_COMP}_${CH4_COMP}_${N2_COMP}_${C2H6_COMP}.input inputs
+./run.sh
+mkdir Out${CO2_COMP}_${CH4_COMP}_${N2_COMP}_${C2H6_COMP}
+cp -r Output Out${CO2_COMP}_${CH4_COMP}_${N2_COMP}_${C2H6_COMP}
+mv Out${CO2_COMP}_${CH4_COMP}_${N2_COMP}_${C2H6_COMP} outputs/
