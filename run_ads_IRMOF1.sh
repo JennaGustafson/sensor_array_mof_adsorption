a0f5169b90981d9bb4792e3064fd1322fc371687
JennaGustafson <jag227@pitt.edu>
2016-04-29 15:34:10 -0400

    added framework as input argument and sed input file

--- run_ads_IRMOF1.sh
+++ run_ads_IRMOF1.sh
@@ -4,9 +4,11 @@ CO2_COMP=$1
 CH4_COMP=$2
 N2_COMP=$3
 C2H6_COMP=$4
+MOF=$5
 
 mkdir -p outputs
 mkdir -p inputs
+sed -i "s/^FrameworkName .*$/FrameworkName $MOF/" simulation.input
 sed -i "20 s/[0-9]*\.[0-9]*/$CO2_COMP/" simulation.input
 sed -i "31 s/[0-9]*\.[0-9]*/$CH4_COMP/" simulation.input
 sed -i "42 s/[0-9]*\.[0-9]*/$N2_COMP/" simulation.input
@@ -14,8 +16,12 @@ sed -i "53 s/[0-9]*\.[0-9]*/$C2H6_COMP/" simulation.input
 
 cp simulation.input sim_${CO2_COMP}_${CH4_COMP}_${N2_COMP}_${C2H6_COMP}.input
 mv sim_${CO2_COMP}_${CH4_COMP}_${N2_COMP}_${C2H6_COMP}.input inputs
-#./run.sh
 simulate simulation.input
+
+mass=$(./calculate_mass.sh ./Output/System_0)
+
 mkdir Out_${CO2_COMP}_${CH4_COMP}_${N2_COMP}_${C2H6_COMP}
 cp -r Output Out_${CO2_COMP}_${CH4_COMP}_${N2_COMP}_${C2H6_COMP}
 mv Out_${CO2_COMP}_${CH4_COMP}_${N2_COMP}_${C2H6_COMP} outputs/
+
+echo $mass
