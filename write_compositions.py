22442abf556cb2edeef7cfc3bfc83c295ec890e8
Paul Boone <narcissus.pong@gmail.com>
2016-05-09 17:50:40 -0400

    refactor to use format similar to HTSOHM codebase (UNTESTED)

--- write_compositions.py
+++ write_compositions.py
@@ -4,6 +4,7 @@
 #read in csv file with mofs
 import csv
 import subprocess
+import sensor_array_mof_adsorption_simulation
 
 def read_mof_configuration(filename):
     with open(filename) as f:
@@ -20,10 +21,23 @@ mofs = read_mof_configuration('mofs.csv')
 print(compositions)
 print(mofs)
 
-with open('comp_mass_output.csv','w',newline='') as f:
-    writer = csv.writer(f, delimiter='\t')
-    writer.writerow(['MOF','CO2','CH4','N2','C2H6','Mass 1bar','Mass 10bar'])
+f = open('comp_mass_output.csv','w',newline='')
+
+# write header
+writer = csv.writer(f, delimiter='\t')
+writer.writerow(['MOF','CO2','CH4','N2','C2H6','Mass 1bar','Mass 10bar'])
 
 for mof in mofs:
     for composition in compositions:
-        subprocess.run(["./run_adsorption.sh",composition['CO2'],composition['CH4'],composition['N2'],composition['C2H6'],mof])
+        mass_p1, mass_p2 = sensor_array_mof_adsorption_simulation.run(
+            mof,
+            composition['CO2'], composition['CH4'], composition['N2'], composition['C2H6']
+        )
+
+        writer.writerow([
+            mof,
+            composition['CO2'], composition['CH4'], composition['N2'], composition['C2H6'],
+            mass_p1, mass_p2
+        ])
+
+f.close()
