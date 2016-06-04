451a6fd1f60e60d00e39b8b35c88b3f9991ee015
Jenna Gustafson <jag227@pitt.edu>
2016-06-04 16:43:42 -0400

    writes simulation input file for each run

--- sensor_array_mof_adsorption_simulation.py
+++ sensor_array_mof_adsorption_simulation.py
@@ -2,20 +2,57 @@ import os
 import subprocess
 import shutil
 
-def write_raspa_file(filename, mof, co2_mf, ch4_mf, n2_mf, c2h6_mf):
-    shutil.copyfile("simulation_template.file",filename)
-    subprocess.run(['sed', '-i', "s/^FrameworkName .*$/FrameworkName %s/" % mof, filename], check=True)
-    subprocess.run(['sed', '-i', "20 s/[0-9]*\.[0-9]*/%s/" % co2_mf,  filename], check=True)
-    subprocess.run(['sed', '-i', "31 s/[0-9]*\.[0-9]*/%s/" % ch4_mf,  filename], check=True)
-    subprocess.run(['sed', '-i', "42 s/[0-9]*\.[0-9]*/%s/" % n2_mf,   filename], check=True)
-    subprocess.run(['sed', '-i', "53 s/[0-9]*\.[0-9]*/%s/" % c2h6_mf, filename], check=True)
+def write_raspa_file(mof, pressure, gases, composition):
+    f = open('simulation.input','w',newline='')
+
+    simulation_file_header = """
+SimulationType                MonteCarlo
+NumberOfCycles                1000
+NumberOfInitializationCycles  1000
+PrintEvery                    100
+
+ChargeMethod                  Ewald
+CutOff                        12.0
+Forcefield                    GenericMOFs
+EwaldPrecision                1e-6
+
+Framework 0
+FrameworkName %s
+UnitCells 1 1 1
+HeliumVoidFraction 0.81
+ExternalTemperature 298.0
+ExternalPressure %s
+""" $(mof, pressure)
+
+    f.write(simulation_file_header)
+
+    component_number = 0
+    for gas in gases:
+        simulation_file_gas = """
+    Component %s MoleculeName              %s
+                 MoleculeDefinition         TraPPE
+                 MolFraction                %s
+                 TranslationProbability     0.5
+                 RegrowProbability          0.5
+                 IdentityChangeProbability  1.0
+                   NumberOfIdentityChanges  2
+                   IdentityChangesList      0 1
+                 SwapProbability            1.0
+                 CreateNumberOfMolecules    0
+
+                 """ $(component_number, gas, mole_fraction)
+
+        f.write(simulation_file_gas)
+        component_number += 1
+
+    f.close()
 
 def parse_output(output_file):
     mass = float(subprocess.check_output(['./calculate_mass.sh', output_file]))
     return mass
 
-def run(mof, co2_mf, ch4_mf, n2_mf, c2h6_mf):
-    write_raspa_file('simulate.input', mof, co2_mf, ch4_mf, n2_mf, c2h6_mf)
+def run(run_descriptor, mof, pressure, gases, composition):
+    write_raspa_file(mof, pressure, gases, composition)
     subprocess.run(['simulate', 'simulate.input'], check=True)
 
     mass_p1 = parse_output('./Output/System_0/*00.data')
