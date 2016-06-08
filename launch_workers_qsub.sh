068d42efe9b71a0e2e646d8ad50ed8b62afe6e3e
Paul Boone <narcissus.pong@gmail.com>
2016-06-08 13:57:36 -0400

    refactor to use the new sjs (subjob server) package now so we avoid duplication across projects at the WilmerLab

--- launch_workers_qsub.sh
+++ launch_workers_qsub.sh
@@ -13,26 +13,22 @@
 #PBS -l mem=1GB
 #PBS -S /bin/bash
 
+# accepts a parameter stay_alive if you don't want the worker to exit immediately after all jobs
+# are complete
+# use like `qsub -v stay_alive=1`
+
 echo JOB_ID: $PBS_JOBID JOB_NAME: $PBS_JOBNAME HOSTNAME: $PBS_O_HOST
 echo start_time: `date`
 
 # dependencies
 module purge
 module load python/3.5.1
+source ~/venv/sensor_ads/bin/activate
 
-cd /ihome/cwilmer/pab135/workspace/sensor_array_mof_adsorption
-
-# must explicitly set LC_ALL and LANG to run `rq worker` because of its dependency on 'Click'
-export LC_ALL=en_US.utf-8
-export LANG=en_US.utf-8
-
-# This file must be run from the ./sensor_array_mof_adsorption directory
-rq worker -u redis://10.201.0.11:6379 sensor_ads
-
-echo end_time: `date`
+cd $PBS_O_WORKDIR
+sjs_launch_workers.sh $PBS_NUM_PPN $stay_alive
 
 # workaround for .out / .err files not always being copied back to $PBS_O_WORKDIR
-
 cp /var/spool/torque/spool/$PBS_JOBID.OU $PBS_O_WORKDIR/$PBS_JOBID$(hostname)_$$.out
 cp /var/spool/torque/spool/$PBS_JOBID.ER $PBS_O_WORKDIR/$PBS_JOBID$(hostname)_$$.err
 
