7df496b2902da14e4b8fea7fe583dbfdf7e31b4c
Jenna Gustafson <jag227@pitt.edu>
2016-06-09 14:23:14 -0400

    refactoring to generalize gases, pmf method finished

--- execute_process_mass_data.py
+++ execute_process_mass_data.py
@@ -16,9 +16,9 @@ mrange = data['mrange']
 gases = data['gases']
 mof_array = data['mof_array']
 
-interpolate_pmf_results = interpolate_pmf(mofs_import, all_results_import, mof_experimental_mass,
-    mof_densities_import, num_mixtures, stdev, mrange)
-create_bins_results = create_bins(interpolate_pmf_results)
+interpolate_data_results = interpolate_data(mofs_import, all_results_import, mof_densities_import, gases)
+calculate_pmf_results = (interpolate_data_results, mofs_import, mof_experimental_mass, stdev, mrange)
+create_bins_results = create_bins(calculate_pmf_results)
 bin_compositions_results = bin_compositions(gases, mof_array, create_bins_results,
     interpolate_pmf_results)
 plot_binned_pmf_array(gases, mof_array, bin_compositions_results, create_bins_results)
