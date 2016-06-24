8734de0104cc2a5396eb88ca9fa5060469b868c0
Jenna Gustafson <jag227@pitt.edu>
2016-06-24 16:18:19 -0400

    updated config file, consolidating dictionaries and adjusted execute file to import the dicts appropriately

--- execute_process_mass_data.py
+++ execute_process_mass_data.py
@@ -4,20 +4,24 @@ from process_mass_data import *
 
 all_results_import = read_output_data('comp_mass_output_tmp.csv')
 
-filepath = 'process_config.yaml'
+filepath = 'process_config.sample.yaml'
 data = yaml_loader(filepath)
 
-mof_densities_import = data['density']
-mofs_import = data['mofs']
-mof_experimental_mass = data['experimental_mass']
+mof_array = data['mof_array']
+mof_densities_import = {}
+mof_experimental_mass = {}
+for mof in mof_array:
+    mof_densities_import.copy()
+    mof_densities_import.update({ mof : data['mofs'][mof]['density']})
+    mof_experimental_mass.copy()
+    mof_experimental_mass.update({ mof : data['mofs'][mof]['experimental_mass'] })
 num_mixtures = data['num_mixtures']
 stdev = data['stdev']
 mrange = data['mrange']
 gases = data['gases']
-mof_array = data['mof_array']
 
-interpolate_data_results = interpolate_data(mofs_import, all_results_import, mof_densities_import, gases)
-calculate_pmf_results = calculate_pmf(interpolate_data_results, mofs_import, mof_experimental_mass, stdev, mrange)
-create_bins_results = create_bins(mofs_import, calculate_pmf_results, gases)
+interpolate_data_results = interpolate_data(mof_array, all_results_import, mof_densities_import, gases)
+calculate_pmf_results = calculate_pmf(interpolate_data_results, mof_array, mof_experimental_mass, stdev, mrange)
+create_bins_results = create_bins(mof_array, calculate_pmf_results, gases)
 bin_compositions_results = bin_compositions(gases, mof_array, create_bins_results, calculate_pmf_results)
 plot_binned_pmf_array(gases, mof_array, bin_compositions_results, create_bins_results)
