0447d3519d5c212ba6b759031556fb9685cb3045
Jenna Gustafson <jag227@pitt.edu>
2017-05-02 14:28:21 -0400

    rewrites unit cell lengths for each simulation input

--- mof_array/sim/sensor_array_mof_adsorption_simulation.py
+++ mof_array/sim/sensor_array_mof_adsorption_simulation.py
@@ -14,6 +14,7 @@ def yaml_loader(filepath):
 def write_raspa_file(filename, mof, pressure, gases, composition, config_file):
     config_data = yaml_loader(config_file)
     gas_names_def = config_data['Forcefield_Gas_Names']
+    framework_unit_cells = config_data['Framework_Unit_Cells'][mof]
     f = open(filename,'w',newline='')
 
     simulation_file_header = """
@@ -29,12 +30,12 @@ EwaldPrecision                1e-6
 
 Framework 0
 FrameworkName %s
-UnitCells 1 1 1
+UnitCells %s
 HeliumVoidFraction 0.81
 UseChargesFromCIFFile yes
 ExternalTemperature 298.0
 ExternalPressure %s
-""" % (mof, pressure)
+""" % (mof, framework_unit_cells, pressure)
 
     f.write(simulation_file_header)
 
