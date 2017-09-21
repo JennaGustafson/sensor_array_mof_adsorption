819607e5c7b7fe7ad9b7c58b409a805ec33dee72
Jenna Gustafson <jag227@pitt.edu>
2017-09-21 15:20:21 -0400

    calculate joint pmf before binning points

--- execute_process_mass_data.py
+++ execute_process_mass_data.py
@@ -41,28 +41,28 @@ number_bins = data['number_bins']
 experimental_mass_results, experimental_mass_mofs, experimental_mofs = import_experimental_results(mof_array, experimental_mass_import, mof_densities_import, gases)
 import_data_results = import_simulated_data(experimental_mofs, all_results_import, mof_densities_import, gases)
 calculate_pmf_results = calculate_pmf(experimental_mass_results, import_data_results, experimental_mofs, mof_experimental_mass, stdev, mrange)
-save_raw_pmf_data(calculate_pmf_results)
+# save_raw_pmf_data(calculate_pmf_results)
 
+array_pmf_results, labeled_exp_mass_mofs, list_of_arrays = array_pmf(gases, number_mofs, experimental_mofs, calculate_pmf_results, experimental_mass_mofs)
 create_bins_results = create_bins(experimental_mofs, calculate_pmf_results, gases, number_bins)
-bin_compositions_results = bin_compositions(gases, experimental_mofs, create_bins_results, calculate_pmf_results, experimental_mass_mofs)
-array_pmf_results, labeled_exp_mass_mofs = array_pmf(gases, number_mofs, experimental_mofs, bin_compositions_results, experimental_mass_mofs)
-plot_binned_pmf_array(gases, experimental_mofs, create_bins_results, array_pmf_results)
-save_array_pmf_data(gases, experimental_mofs, create_bins_results, array_pmf_results)
-kl_divergence = information_gain(array_pmf_results, create_bins_results, labeled_exp_mass_mofs)
+bin_compositions_results = bin_compositions(gases, list_of_arrays, create_bins_results, array_pmf_results, experimental_mass_mofs)
+# plot_binned_pmf_array(gases, list_of_arrays, create_bins_results, bin_compositions_results)
+# save_array_pmf_data(gases, list_of_arrays, create_bins_results, bin_compositions_results)
+kl_divergence = information_gain(gases, list_of_arrays, bin_compositions_results, create_bins_results)
 combined_kld, ordered_by_gas, best_arrays, ordered_kld_w_array, average_kld = choose_best_arrays(gases, kl_divergence)
 
-write_output_data('saved_results/ordered_by_kld_product_%s.csv' % (datetime.now().strftime("%Y_%m_%d__%H_%M_%S")), combined_kld)
-write_output_data('saved_results/ordered_by_gas_kld_%s.csv' % (datetime.now().strftime("%Y_%m_%d__%H_%M_%S")), ordered_by_gas)
-
-# Print results, including the "best" MOF array structures
-print(' ================ RESULTS ===============')
-print("\nThe MOF array with the highest informaion content, of %s, is: %s \n" % (str(ordered_kld_w_array[0]['KLD']),
-                                                                                 str(ordered_kld_w_array[0]['mof array'])))
-print("\nThe MOF array with the lowest informaion content, of %s, is: %s \n" % (str(ordered_kld_w_array[len(ordered_kld_w_array)-1]['KLD']),
-                                                                                str(ordered_kld_w_array[len(ordered_kld_w_array)-1]['mof array'])))
-
-for result in best_arrays:
-    print("The best array for %s consists of MOFs: %s" % (result['gas'], result['mof array']))
-print('\nThe average Kullback-Liebler Divergence for a: ')
-for num_mofs in range(0,len(average_kld)):
-    print('\t%s MOF array is %s' %(num_mofs + 1, average_kld[num_mofs]))
+# write_output_data('saved_results/ordered_by_kld_product_%s.csv' % (datetime.now().strftime("%Y_%m_%d__%H_%M_%S")), combined_kld)
+# write_output_data('saved_results/ordered_by_gas_kld_%s.csv' % (datetime.now().strftime("%Y_%m_%d__%H_%M_%S")), ordered_by_gas)
+#
+# # Print results, including the "best" MOF array structures
+# print(' ================ RESULTS ===============')
+# print("\nThe MOF array with the highest informaion content, of %s, is: %s \n" % (str(ordered_kld_w_array[0]['KLD']),
+#                                                                                  str(ordered_kld_w_array[0]['mof array'])))
+# print("\nThe MOF array with the lowest informaion content, of %s, is: %s \n" % (str(ordered_kld_w_array[len(ordered_kld_w_array)-1]['KLD']),
+#                                                                                 str(ordered_kld_w_array[len(ordered_kld_w_array)-1]['mof array'])))
+#
+# for result in best_arrays:
+#     print("The best array for %s consists of MOFs: %s" % (result['gas'], result['mof array']))
+# print('\nThe average Kullback-Liebler Divergence for a: ')
+# for num_mofs in range(0,len(average_kld)):
+#     print('\t%s MOF array is %s' %(num_mofs + 1, average_kld[num_mofs]))
